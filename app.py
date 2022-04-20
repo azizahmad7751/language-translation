e73a9177cc3f25da14ba4b733daed8da42055439
Aziz Ahmad <55359922+caps7751@users.noreply.github.com>
2022-04-21 01:39:46 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-from transformers import MBartForConditionalGeneration, MBart50Tokenizer
+from transformers import MBartForConditionalGeneration, MBart50Tokenizer, MarianTokenizer, MarianMTModel
 import streamlit as st
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
+
 def download_model():
-    model_name = "facebook/mbart-large-50-many-to-many-mmt"
-    model = MBartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = MBart50Tokenizer.from_pretrained(model_name)
+    model_name = f'Helsinki-NLP/opus-mt-en-ur'
+    model = MarianMTModel.from_pretrained(model_name)
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
     return model, tokenizer
 
-st.title('English to Pashto Translator ')
+st.title('English to Urdu Translator')
 st.markdown("[Developd By: Aziz Ahmad](https://www.linkedin.com/in/theazizahmad/)", unsafe_allow_html=True)
 
 text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
 model, tokenizer = download_model()
 
-if st.button('Translate to Pashto'):
+if st.button('Translate to Urdu'):
     if text == '':
         st.write('Please enter English text for translation') 
     else: 
-        model_name = "facebook/mbart-large-50-many-to-many-mmt"
-        tokenizer.src_lang = "en_IN"
-        encoded_hindi_text = tokenizer(text, return_tensors="pt")
-        generated_tokens = model.generate(**encoded_hindi_text, forced_bos_token_id=tokenizer.lang_code_to_id["ps_XX"])
-        out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-        st.write('', str(out).strip('][\''))
+      
+         # Tokenize the text
+        batch = tokenizer(text, return_tensors="pt", padding=True)
+        # tokenized text maximum allowed size of 512
+        batch["input_ids"] = batch["input_ids"][:, :512]
+        batch["attention_mask"] = batch["attention_mask"][:, :512]
+        translation_encoded = model.generate(**batch)
+        translation = tokenizer.batch_decode(translation_encoded, skip_special_tokens=True)
+        st.write('', str(translation).strip('][\''))
 else: pass
